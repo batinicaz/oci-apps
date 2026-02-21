b91dca9606874084cb58e7e7da85493a16d5908a
batinica <batinicaz@users.noreply.github.com>
2026-02-21 19:11:25 +0000

    fix: remove redundant chown

--- os-config/scripts/languagetool-ngrams.sh
+++ os-config/scripts/languagetool-ngrams.sh
@@ -10,4 +10,3 @@ mkdir -p "$NGRAM_DIR"
 
 curl -fSL -o "$ZIPFILE" "https://languagetool.org/download/ngram-data/ngrams-en-20150817.zip"
 bsdtar -xf "$ZIPFILE" -C "$NGRAM_DIR"
-chown -R 783:783 "$NGRAM_DIR"
