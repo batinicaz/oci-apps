fc2010027499fa425be1594168f8d738df044fff
batinica <batinicaz@users.noreply.github.com>
2026-03-29 14:49:00 +0100

    fix: oci cli authentication

--- terraform/scripts/upload-fcos.sh
+++ terraform/scripts/upload-fcos.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 command -v oci >/dev/null || pip install -q oci-cli
 
+export OCI_CLI_USER="${TF_VAR_oci_user_id:?}"
+export OCI_CLI_FINGERPRINT="${TF_VAR_oci_fingerprint:?}"
+export OCI_CLI_TENANCY="${TF_VAR_oci_tenancy_id:?}"
+export OCI_CLI_REGION="${TF_VAR_oci_region:?}"
+export OCI_CLI_KEY_CONTENT="${TF_VAR_oci_private_key:?}"
+
 : "${FCOS_VERSION:?}"
 : "${FCOS_BUILD_URL:?}"
 : "${FCOS_SHA256:?}"
