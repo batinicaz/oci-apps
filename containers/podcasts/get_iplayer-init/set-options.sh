1c4fb87c0bebff1ee1e441c2055123f0db4a8b6e
batinicaz <102460506+batinicaz@users.noreply.github.com>
2026-04-03 11:08:16 +0100

    fix: chown on podcasts directory (#142)
    Co-authored-by: batinica <batinicaz@users.noreply.github.com>

--- containers/podcasts/get_iplayer-init/set-options.sh
+++ containers/podcasts/get_iplayer-init/set-options.sh
@@ -2,7 +2,7 @@
 # Ensure required get_iplayer preferences are set on every startup.
 # Resets command-radio on every startup to ensure changes are picked up.
 mkdir -p /tmp/getiplayer-staging /podcasts/bbc
-chown abc:abc /tmp/getiplayer-staging /podcasts/bbc
+chown -R abc:abc /tmp/getiplayer-staging /podcasts/bbc
 get_iplayer --profile-dir /config/.get_iplayer --prefs-del --command-radio 2>/dev/null
 get_iplayer --profile-dir /config/.get_iplayer --prefs-add \
   --output /tmp/getiplayer-staging \
